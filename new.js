e0026f50a03c7b3d34ede9889b1ac40a4a325ba5
Gloryk100 <ihotuitodo100@gmail.com>
2026-07-22 17:01:05 +0100

    Add JS to update paragraph and image attributes

---
+++ new.js
@@ -0,0 +1,17 @@
+
+const paragraph = document.getElementById('myParagraph');
+const image = document.getElementById('myImage');
+
+
+paragraph.textContent = 'This is the updated paragraph text.';
+paragraph.style.color = 'blue';
+paragraph.id = 'updatedParagraph';
+
+
+image.src = 'new-image.jpg';
+image.alt = 'A description of the new image';
+image.width = 400;
+image.height = 300;
+image.title = 'This is the new image title';
+
+
